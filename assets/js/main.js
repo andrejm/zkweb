f0b2e24988ffc1e01686e067a366f930a1789fef
Martin <martin.greksa@gmail.com>
2018-11-14 23:21:54 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	// $(document).foundation();
+})(jQuery);
+
+jQuery(document).ready(function($) {
+
+	/*
+	* Initialize Foundation Sites plugins
+	*/
+
+	// $(document).foundation();
+
+	/*
+	* SVG 4 everybody
+	*/
+
+	// svg4everybody({
+	//     polyfill: true // polyfill <use> elements for External Content
+	// });
+
+});
+
+
+
